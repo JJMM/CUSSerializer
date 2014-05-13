b5f27795e0fd96bb0e9c9d69eb2873d916103625
zhangyu <zhangyu@jiuqi.com.cn>
2014-05-13 16:28:30 +0800

    create
    create

--- CUSSerializerExample/controller/CUSMainViewController.m
+++ CUSSerializerExample/controller/CUSMainViewController.m
@@ -41,65 +41,65 @@
     NSString *path = [[NSBundle mainBundle] pathForResource:@"DictionaryFile" ofType:@"plist"];
     NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:path];
     CUSSchool *school = [dictionary deserialize];
-    
-    NSLog(@"school:%@",school);
     NSDictionary *serializeDic = [school serialize];
+    NSLog(@"school:%@",school);
+    
 
     self.textView.text = [NSString stringWithFormat:@"%@",serializeDic];
 }
 
 ////////////////////Example code////////////////////
--(CUSSchool *)createSchool{
-    CUSSchool *school = [[CUSSchool alloc]init];
-    school.schoolId = @"id0123456";
-    school.schoolName = @"qinghuadaxue";
-    school.schoolAddress = nil;
-    school.schoolPost = 123;
-    school.schoolFloat = 1234.5678;
-    school.boolValue = YES;
-    school.ch = 's';
-    school.schoolNumber = [NSNumber numberWithInt:6];
-    school.schoolDate = [NSDate date];
-    school.headmaster = [self createTeacher:99];
-    NSMutableArray *teacherArray = [NSMutableArray array];
-    for (int i = 0; i < 3; i++) {
-        [teacherArray addObject:[self createTeacher:i]];
-    }
-    school.teacherArray = teacherArray;
-    
-    NSMutableDictionary *teacherDictionary = [NSMutableDictionary dictionary];
-    for (int i = 0; i < 5; i++) {
-        NSString *key = [NSString stringWithFormat:@"key%i",i];
-        [teacherDictionary setObject:[self createTeacher:i] forKey:key];
-    }
-    school.teacherDictionary = teacherDictionary;
-    return school;
-}
--(CUSTeacher *)createTeacher:(NSInteger)index{
-    CUSTeacher *model = [[CUSTeacher alloc]init];
-    model.iden = [NSString stringWithFormat:@"teacherId%i",index];
-    model.name = [NSString stringWithFormat:@"teacherName%i",index];
-    model.age = 30 +index;
-    model.courseName = [NSString stringWithFormat:@"courseName%i",index];
-    
-    NSMutableArray *array = [NSMutableArray array];
-    for (int i = 0; i < 2; i++) {
-        [array addObject:[self createStudent:i]];
-    }
-    model.studentArray = array;
-    
-    return model;
-}
--(CUSStudent *)createStudent:(NSInteger)index{
-    CUSStudent *model = [[CUSStudent alloc]init];
-    model.iden = [NSString stringWithFormat:@"studentId%i",index];
-    model.name = [NSString stringWithFormat:@"studentName%i",index];
-    model.age = 15 +index;
-    return model;
-}
-
--(void)dicWriteToFile:(NSDictionary *)dic{
-    NSString *filePath = [NSString stringWithFormat:@"%@/Documents/DictionaryFile.plist",NSHomeDirectory()];
-    [dic writeToFile:filePath atomically:YES];
-}
+//-(CUSSchool *)createSchool{
+//    CUSSchool *school = [[CUSSchool alloc]init];
+//    school.schoolId = @"id0123456";
+//    school.schoolName = @"qinghuadaxue";
+//    school.schoolAddress = nil;
+//    school.schoolPost = 123;
+//    school.schoolFloat = 1234.5678;
+//    school.boolValue = YES;
+//    school.ch = 's';
+//    school.schoolNumber = [NSNumber numberWithInt:6];
+//    school.schoolDate = [NSDate date];
+//    school.headmaster = [self createTeacher:99];
+//    NSMutableArray *teacherArray = [NSMutableArray array];
+//    for (int i = 0; i < 3; i++) {
+//        [teacherArray addObject:[self createTeacher:i]];
+//    }
+//    school.teacherArray = teacherArray;
+//    
+//    NSMutableDictionary *teacherDictionary = [NSMutableDictionary dictionary];
+//    for (int i = 0; i < 5; i++) {
+//        NSString *key = [NSString stringWithFormat:@"key%i",i];
+//        [teacherDictionary setObject:[self createTeacher:i] forKey:key];
+//    }
+//    school.teacherDictionary = teacherDictionary;
+//    return school;
+//}
+//-(CUSTeacher *)createTeacher:(NSInteger)index{
+//    CUSTeacher *model = [[CUSTeacher alloc]init];
+//    model.iden = [NSString stringWithFormat:@"teacherId%i",index];
+//    model.name = [NSString stringWithFormat:@"teacherName%i",index];
+//    model.age = 30 +index;
+//    model.courseName = [NSString stringWithFormat:@"courseName%i",index];
+//    
+//    NSMutableArray *array = [NSMutableArray array];
+//    for (int i = 0; i < 2; i++) {
+//        [array addObject:[self createStudent:i]];
+//    }
+//    model.studentArray = array;
+//    
+//    return model;
+//}
+//-(CUSStudent *)createStudent:(NSInteger)index{
+//    CUSStudent *model = [[CUSStudent alloc]init];
+//    model.iden = [NSString stringWithFormat:@"studentId%i",index];
+//    model.name = [NSString stringWithFormat:@"studentName%i",index];
+//    model.age = 15 +index;
+//    return model;
+//}
+//
+//-(void)dicWriteToFile:(NSDictionary *)dic{
+//    NSString *filePath = [NSString stringWithFormat:@"%@/Documents/DictionaryFile.plist",NSHomeDirectory()];
+//    [dic writeToFile:filePath atomically:YES];
+//}
 @end
--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-CUSSerializer
-=============
+Automatic serialization tool,convert model to NSDictionary, support recursive conversion NSDictionary, NSArray data structures.<br>
+![image](https://github.com/JJMM/CUSResources/raw/master/CUSSerializer.PNG)
 
-Automatic serialization tool,convert model to NSDictionary, support recursive conversion NSDictionary, NSArray data structures, In addition, it's very suitable for use with JSONKit
+## How To Get Started
+1.Copy the folder named CUSSerializer to your project
+
+## serialize
+Only one line of code recursive convert model to NSDictionary.
+```objective-c
+CUSSchool *school = [[CUSSchool alloc]init];
+NSDictionary *serializeDic = [school serialize];
+```
+## deserialize
+Only one line of code recursive convert NSDictionary to model.
+```objective-c
+NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:path];
+CUSSchool *school = [dictionary deserialize];
+```
+## Mapping
+The class name is mapped to another symbol for many types of client
+```objective-c
+[CUSSerializer setClassMapping:@"CUSSchool" mappingFor:@"School"];
+[CUSSerializer setClassMapping:@"CUSTeacher" mappingFor:@"Teacher"];
+[CUSSerializer setClassMapping:@"CUSStudent" mappingFor:@"Student"];
+```
+## LOG
+open or close Log
+```objective-c
+[CUSSerializer setLogStatus:NO];
+```
+## License
+CUSSerializer is licensed under the terms of the [Apache License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html). Please see the [LICENSE](LICENSE) file for full details.
+
+## Contributions
+
+Contributions are totally welcome. We'll review all pull requests and if you send us a good one/are interested we're happy to give you push access to the repo. Or, you know, you could just come work with us.<br>
+
+Please pay attention to add Star, your support is my greatest motivation, thank you.
